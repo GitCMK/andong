f4046891fed64e399a60fae1fb9d4faa7d003047
GitCMK <choreo4444@gmail.com>
2024-10-19 11:29:01 +0900

    first commit

---
+++ textfile.py
@@ -0,0 +1,4 @@
+def addHello(string):
+    return string + "Hello"
+def addBye(string):
+    return string + "Bye"
---
+++ textfile2.py
@@ -0,0 +1,4 @@
+from textfile2 import addHello, addBye
+string = "seongho"
+print(addHello(string))
+print(addBye(string))
